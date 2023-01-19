a7689875b6fa28d9a2be570f5868019e87e257f8
9FS <pray4spam@googlemail.com>
2023-01-19 23:25:57 +0000

    updated KFS library and requirements.txt

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import datetime as dt
 import dropbox
 import dropbox.exceptions
 import time
-from KFS import KFSlog
+import KFS.log
 from dropbox_list_files import dropbox_list_files
 
 
-@KFSlog.timeit
+@KFS.log.timeit
 def main():
     backups=None                                    #backup file names
     BACKUPS_PATH="/Apps/Logbook.aero/"              #path to backup files, can't delete folder afterwards because link to Logbook.aero gets destroyed
@@ -17,10 +17,10 @@ def main():
     REFRESH_RATE=1/100                              #work with 10mHz (every 100s)
 
 
-    KFSlog.write("Loading dropbox API token...")
+    KFS.log.write("Loading dropbox API token...")
     with open("dropbox_API_token.txt", "rt") as dropbox_API_token_file:
         DROPBOX_API_TOKEN=dropbox_API_token_file.read() #read API access token
-    KFSlog.write("\rLoaded dropbox API token.")
+    KFS.log.write("\rLoaded dropbox API token.")
 
     dbx=dropbox.Dropbox(DROPBOX_API_TOKEN)  #create Dropbox instance
     
@@ -34,28 +34,28 @@ def main():
         try:
             backups=dropbox_list_files(dbx, BACKUPS_PATH)   #download content list of backup folder
         except dropbox.exceptions.ApiError:                 #if folder does not exist: do nothing
-            KFSlog.write(f"\"Dropbox{BACKUPS_PATH}\" does not exist. Not doing anything...")
+            KFS.log.write(f"\"Dropbox{BACKUPS_PATH}\" does not exist. Not doing anything...")
             continue
 
         if len(backups)==0: #if backups folder is empty: delete
-            KFSlog.write(f"\"Dropbox{BACKUPS_PATH}\" is empty. Not doing anything...")
+            KFS.log.write(f"\"Dropbox{BACKUPS_PATH}\" is empty. Not doing anything...")
             continue
         else:
-            KFSlog.write(f"\"Dropbox{BACKUPS_PATH}\" has contents. Preparing movement.")
+            KFS.log.write(f"\"Dropbox{BACKUPS_PATH}\" has contents. Preparing movement.")
 
-        KFSlog.write(f"Deleting existing backup file from today in \"Dropbox{DEST_PATH}\"...") #make room for new backup file first, move can't overwrite
+        KFS.log.write(f"Deleting existing backup file from today in \"Dropbox{DEST_PATH}\"...") #make room for new backup file first, move can't overwrite
         try:
             dbx.files_delete_v2(f"{DEST_PATH}{dt.datetime.now(dt.timezone.utc).strftime('%Y-%m-%d')} Logbook.csv")
         except dropbox.exceptions.ApiError: #TODO What if file exists and deleting failed because of permission error or something?
-            KFSlog.write(f"Backup file from today does not exist yet in \"Dropbox{DEST_PATH}\".")
+            KFS.log.write(f"Backup file from today does not exist yet in \"Dropbox{DEST_PATH}\".")
         else:
-            KFSlog.write(f"\rDeleted existing backup file from today in \"Dropbox{DEST_PATH}\".")
+            KFS.log.write(f"\rDeleted existing backup file from today in \"Dropbox{DEST_PATH}\".")
 
 
-        KFSlog.write(f"Moving latest backup file \"{backups[-1]}\" from \"Dropbox{BACKUPS_PATH}\" to \"Dropbox{DEST_PATH}\"...")
+        KFS.log.write(f"Moving latest backup file \"{backups[-1]}\" from \"Dropbox{BACKUPS_PATH}\" to \"Dropbox{DEST_PATH}\"...")
         try:
             dbx.files_move_v2(f"{BACKUPS_PATH}{backups[-1]}", f"{DEST_PATH}{dt.datetime.now(dt.timezone.utc).strftime('%Y-%m-%d')} Logbook.csv")
         except dropbox.exceptions.ApiError:
-            KFSlog.write(f"Moving latest backup file \"{backups[-1]}\" from \"Dropbox{BACKUPS_PATH}\" to \"Dropbox{DEST_PATH}\" failed. Not doing anything...")
+            KFS.log.write(f"Moving latest backup file \"{backups[-1]}\" from \"Dropbox{BACKUPS_PATH}\" to \"Dropbox{DEST_PATH}\" failed. Not doing anything...")
             continue
-        KFSlog.write(f"\rMoved latest backup file \"{backups[-1]}\" from \"Dropbox{BACKUPS_PATH}\" to \"Dropbox{DEST_PATH}\".")
+        KFS.log.write(f"\rMoved latest backup file \"{backups[-1]}\" from \"Dropbox{BACKUPS_PATH}\" to \"Dropbox{DEST_PATH}\".")
--- main_outer.py
+++ main_outer.py
@@ -1,12 +1,12 @@
 import traceback    #Exceptionnachricht vollständig wenn Programm als .exe abschmiert
-from KFS import KFSlog
+import KFS.log
 from main import main
 
 
 try:
     main()
 except:
-    KFSlog.write(f"CRASH\n{traceback.format_exc()}")
+    KFS.log.write(traceback.format_exc())
 
     print("\n\nPress enter to close program.")
     input() #pause
