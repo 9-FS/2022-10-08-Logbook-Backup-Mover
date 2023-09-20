d3396ffae8bd272174d8885fdc7f1f261993f6bb
9FS <pray4spam@googlemail.com>
2023-09-20 00:17:02 +0000

    changed "#comment" to "# comment"

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-#Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
+# Copyright (c) 2023 구FS, all rights reserved. Subject to the MIT licence in `licence.md`.
 import datetime as dt
 import dropbox
 import dropbox.exceptions
@@ -11,47 +11,47 @@ import time
 
 @KFS.log.timeit
 def main(logger: logging.Logger) -> None:
-    dbx: dropbox.Dropbox                                        #dropbox instance
-    dest_dir_filenames: list[str]                               #filenames in destination directory
-    dest_filepath: str                                          #destination filepath
-    DEST_PATH="/Documents/Aviation/Logbook/"                    #destination path for backup files
-    DROPBOX_API_CRED: dict[str, str]                            #dropbox API access credentials
-    DROPBOX_CONFIG_CONTENT_DEFAULT: dict[str, str]={            #dropbox configuration default content
+    dbx: dropbox.Dropbox                                        # dropbox instance
+    dest_dir_filenames: list[str]                               # filenames in destination directory
+    dest_filepath: str                                          # destination filepath
+    DEST_PATH="/Documents/Aviation/Logbook/"                    # destination path for backup files
+    DROPBOX_API_CRED: dict[str, str]                            # dropbox API access credentials
+    DROPBOX_CONFIG_CONTENT_DEFAULT: dict[str, str]={            # dropbox configuration default content
         "app_key": "",
         "app_secret": "",
         "refresh_token": "",
     }
-    exec_next_DT: dt.datetime=dt.datetime.now(dt.timezone.utc)  #next execution
-    refresh_rate: float                                         #how often to refresh
-    source_dir_filenames: list[str]                             #filenames in source directory
-    source_filepath: str                                        #source filepath
-    SOURCE_PATH="/Apps/Logbook.aero/"                           #path to backup files, can't delete folder afterwards because link to Logbook.aero gets destroyed
+    exec_next_DT: dt.datetime=dt.datetime.now(dt.timezone.utc)  # next execution
+    refresh_rate: float                                         # how often to refresh
+    source_dir_filenames: list[str]                             # filenames in source directory
+    source_filepath: str                                        # source filepath
+    SOURCE_PATH="/Apps/Logbook.aero/"                           # path to backup files, can't delete folder afterwards because link to Logbook.aero gets destroyed
 
 
-    if logger.level<=logging.DEBUG: #if debug mode:
-        refresh_rate=1/10           #refresh with 100mHz (every 10s)
-    else:                           #if normal mode:
-        refresh_rate=1/100          #refresh with 10mHz (every 100s)
+    if logger.level<=logging.DEBUG: # if debug mode:
+        refresh_rate=1/10           # refresh with 100mHz (every 10s)
+    else:                           # if normal mode:
+        refresh_rate=1/100          # refresh with 10mHz (every 100s)
     try:
-        DROPBOX_API_CRED=json.loads(KFS.config.load_config("dropbox_API.json", json.dumps(DROPBOX_CONFIG_CONTENT_DEFAULT, indent=4)))                           #load API credentials
+        DROPBOX_API_CRED=json.loads(KFS.config.load_config("dropbox_API.json", json.dumps(DROPBOX_CONFIG_CONTENT_DEFAULT, indent=4)))                           # load API credentials
     except FileNotFoundError:
         return
-    dbx=dropbox.Dropbox(oauth2_refresh_token=DROPBOX_API_CRED["refresh_token"], app_key=DROPBOX_API_CRED["app_key"], app_secret=DROPBOX_API_CRED["app_secret"]) #create Dropbox instance
+    dbx=dropbox.Dropbox(oauth2_refresh_token=DROPBOX_API_CRED["refresh_token"], app_key=DROPBOX_API_CRED["app_key"], app_secret=DROPBOX_API_CRED["app_secret"]) # create Dropbox instance
     
 
     while True:
         while dt.datetime.now(dt.timezone.utc)<=exec_next_DT:
             time.sleep(1)
-        exec_next_DT=dt.datetime.now(dt.timezone.utc)+dt.timedelta(seconds=1/refresh_rate-exec_next_DT.timestamp()%(1/refresh_rate))    #when refresh next time?
+        exec_next_DT=dt.datetime.now(dt.timezone.utc)+dt.timedelta(seconds=1/refresh_rate-exec_next_DT.timestamp()%(1/refresh_rate))    # when refresh next time?
 
 
         try:
-            source_dir_filenames=KFS.dropbox.list_files(dbx, SOURCE_PATH)   #download content list of backup source folder
-        except dropbox.exceptions.ApiError:                                 #if folder does not exist: do nothing
+            source_dir_filenames=KFS.dropbox.list_files(dbx, SOURCE_PATH)   # download content list of backup source folder
+        except dropbox.exceptions.ApiError:                                 # if folder does not exist: do nothing
             logger.error(f"Source directory \"Dropbox{SOURCE_PATH}\" does not exist. Unable to work. Shutdown...")
             break
 
-        if len(source_dir_filenames)==0: #if backups folder is empty: not doing anything
+        if len(source_dir_filenames)==0: # if backups folder is empty: not doing anything
             logger.info(f"Source directory \"Dropbox{SOURCE_PATH}\" is empty. Waiting {KFS.fstr.notation_tech(1/refresh_rate, 2)}s...")
             continue
         logger.info(f"Source directory \"Dropbox{SOURCE_PATH}\" has contents. Preparing movement.")
@@ -62,16 +62,16 @@ def main(logger: logging.Logger) -> None:
         logger.debug(f"Destination filepath: \"{dest_filepath}\"")
         
         try:
-            dest_dir_filenames=KFS.dropbox.list_files(dbx, DEST_PATH)   #download content list of backup destination folder
-        except dropbox.exceptions.ApiError:                             #if folder does not exist: do nothing
+            dest_dir_filenames=KFS.dropbox.list_files(dbx, DEST_PATH)   # download content list of backup destination folder
+        except dropbox.exceptions.ApiError:                             # if folder does not exist: do nothing
             logger.warning(f"Destination directory filenames could not be downloaded. Source directory \"Dropbox{DEST_PATH}\" probably does not exist yet.")
-            dest_dir_filenames=[]                                       #default empty list
+            dest_dir_filenames=[]                                       # default empty list
         
-        if os.path.basename(dest_filepath) in dest_dir_filenames:                                   #if backup file already exists:
-            logger.info(f"Deleting existing backup file from today \"Dropbox{dest_filepath}\"...")  #make room for new backup file first, move can't overwrite
+        if os.path.basename(dest_filepath) in dest_dir_filenames:                                   # if backup file already exists:
+            logger.info(f"Deleting existing backup file from today \"Dropbox{dest_filepath}\"...")  # make room for new backup file first, move can't overwrite
             try:
                 dbx.files_delete_v2(dest_filepath)
-            except dropbox.exceptions.ApiError: #if deleting unsuccessful: give up this time
+            except dropbox.exceptions.ApiError: # if deleting unsuccessful: give up this time
                 logger.error(f"Deleting existing backup file from today \"Dropbox{dest_filepath}\" failed. Aborting movement...")
                 continue
             else:
